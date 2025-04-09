16738f077c9905776f364f1e6e4f80885fae8460
User <User@DESKTOP-MQKRDP3>
2025-04-09 12:31:46 +0530

    Add project files.

---
+++ WebApp.Sales/Controllers/SalesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Sales.Models;
+
+namespace WebApp.Sales.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee employee = new Employee
+            {
+                Id = 100,
+                Name = "Thomas",
+                Age = 22
+            };
+            return View(employee);
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+       }
+}
